5ceff3d73448309ee704c6e2a1affe9c71f98e90
jvitorfranca <vitoorbr@gmail.com>
2018-09-03 11:36:57 -0300

    case 2

--- school_violence/school_violence/spiders/cases.py
+++ school_violence/school_violence/spiders/cases.py
@@ -6,14 +6,14 @@ class CasesSpider(scrapy.Spider):
     name = "cases"
 
     start_urls = (
-        'http://www.focoregional.com.br/Noticia/caso-de-professora-agredida-em-sc-ganha-reper',
+        'http://g1.globo.com/sao-paulo/sao-jose-do-rio-preto-aracatuba/noticia/2016/03/estudante-agride-professor-apos-pedido-para-desligar-celular-na-aula.html',
     )
 
     def parse(self, response):
         self.log('I just visited: ' + response.url)
         item = {
-            'title': response.css('p.titlenoticiadestaque::text').extract(),
-            'date': response.css('span.atualizadonoticia > span::text').extract(),
-            'text': response.css('div.textonoticia > p::text').extract(),
+            'title': response.css("h1.entry-title::text").extract_first(),
+            'date': response.css("abbr.published::text").extract_first(),
+            'text': response.css("div > p::text").extract(),
         }
         yield item
