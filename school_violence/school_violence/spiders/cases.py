93df028ca9221456732289af6cca194ef917dbcf
jvitorfranca <vitoorbr@gmail.com>
2018-09-04 10:01:49 -0300

    novo link caso 2

--- school_violence/school_violence/spiders/cases.py
+++ school_violence/school_violence/spiders/cases.py
@@ -6,13 +6,14 @@ class CasesSpider(scrapy.Spider):
     name = "cases"
 
     start_urls = (
-        'http://www.apeoesp.org.br/publicacoes/observatorio-da-violencia/estudante-agride-professor-apos-pedido-para-desligar-celular-na-aula/',
+        'http://riopretonorte.com.br/professor-agredido-por-estudante-preparava-seminario-contra-violencia/',
     )
 
     def parse(self, response):
         self.log('I just visited: ' + response.url)
         item = {
             'title': response.css("h1::text").extract_first(),
-            'text': response.css("div.materia-assinatura > p").extract(),
+            'date': response.css("time::text").extract_first(),
+            'text': response.css("div.materia-titulo > p::text").extract(),
         }
         yield item
